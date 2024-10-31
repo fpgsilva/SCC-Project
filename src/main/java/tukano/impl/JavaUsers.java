91d947faca9cd9dce1d42fcbb21d9ff0af03a38c
Francisco <fpg.silva@campus.fct.unl.pt>
2024-10-31 12:11:08 +0000

    Lab 2 Code
    Tukano into CosmosDB

--- src/main/java/tukano/impl/JavaUsers.java
+++ src/main/java/tukano/impl/JavaUsers.java
@@ -12,43 +12,61 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
+import com.azure.cosmos.ConsistencyLevel;
+import com.azure.cosmos.CosmosClient;
+import com.azure.cosmos.CosmosClientBuilder;
+
 import tukano.api.Result;
 import tukano.api.User;
 import tukano.api.Users;
 import utils.DB;
 
 public class JavaUsers implements Users {
-	
+
+	private static final String CONNECTION_URL = "..."; // Replace with values from the Azure portal
+	private static final String DB_KEY = "...";
+	private static final String DB_NAME = "...";
+
+	CosmosClient client = new CosmosClientBuilder()
+			.endpoint(CONNECTION_URL)
+			.key(DB_KEY)
+			.directMode() // comment this if not to use direct mode
+			.consistencyLevel(ConsistencyLevel.SESSION)
+			.connectionSharingAcrossClientsEnabled(true)
+			.contentResponseOnWriteEnabled(true) // On write, return the object written
+			.buildClient();
+
 	private static Logger Log = Logger.getLogger(JavaUsers.class.getName());
 
 	private static Users instance;
-	
+
 	synchronized public static Users getInstance() {
-		if( instance == null )
+		if (instance == null)
 			instance = new JavaUsers();
 		return instance;
 	}
-	
-	private JavaUsers() {}
-	
+
+	private JavaUsers() {
+	}
+
 	@Override
 	public Result<String> createUser(User user) {
 		Log.info(() -> format("createUser : %s\n", user));
 
-		if( badUserInfo( user ) )
-				return error(BAD_REQUEST);
+		if (badUserInfo(user))
+			return error(BAD_REQUEST);
 
-		return errorOrValue( DB.insertOne( user), user.getUserId() );
+		return errorOrValue(DB.insertOne(user), user.getUserId());
 	}
 
 	@Override
 	public Result<User> getUser(String userId, String pwd) {
-		Log.info( () -> format("getUser : userId = %s, pwd = %s\n", userId, pwd));
+		Log.info(() -> format("getUser : userId = %s, pwd = %s\n", userId, pwd));
 
 		if (userId == null)
 			return error(BAD_REQUEST);
-		
-		return validatedUserOrError( DB.getOne( userId, User.class), pwd);
+
+		return validatedUserOrError(DB.getOne(userId, User.class), pwd);
 	}
 
 	@Override
@@ -58,31 +76,32 @@ public class JavaUsers implements Users {
 		if (badUpdateUserInfo(userId, pwd, other))
 			return error(BAD_REQUEST);
 
-		return errorOrResult( validatedUserOrError(DB.getOne( userId, User.class), pwd), user -> DB.updateOne( user.updateFrom(other)));
+		return errorOrResult(validatedUserOrError(DB.getOne(userId, User.class), pwd),
+				user -> DB.updateOne(user.updateFrom(other)));
 	}
 
 	@Override
 	public Result<User> deleteUser(String userId, String pwd) {
 		Log.info(() -> format("deleteUser : userId = %s, pwd = %s\n", userId, pwd));
 
-		if (userId == null || pwd == null )
+		if (userId == null || pwd == null)
 			return error(BAD_REQUEST);
 
-		return errorOrResult( validatedUserOrError(DB.getOne( userId, User.class), pwd), user -> {
+		return errorOrResult(validatedUserOrError(DB.getOne(userId, User.class), pwd), user -> {
 
 			// Delete user shorts and related info asynchronously in a separate thread
-			Executors.defaultThreadFactory().newThread( () -> {
+			Executors.defaultThreadFactory().newThread(() -> {
 				JavaShorts.getInstance().deleteAllShorts(userId, pwd, Token.get(userId));
 				JavaBlobs.getInstance().deleteAllBlobs(userId, Token.get(userId));
 			}).start();
-			
-			return DB.deleteOne( user);
+
+			return DB.deleteOne(user);
 		});
 	}
 
 	@Override
 	public Result<List<User>> searchUsers(String pattern) {
-		Log.info( () -> format("searchUsers : patterns = %s\n", pattern));
+		Log.info(() -> format("searchUsers : patterns = %s\n", pattern));
 
 		var query = format("SELECT * FROM User u WHERE UPPER(u.userId) LIKE '%%%s%%'", pattern.toUpperCase());
 		var hits = DB.sql(query, User.class)
@@ -93,19 +112,18 @@ public class JavaUsers implements Users {
 		return ok(hits);
 	}
 
-	
-	private Result<User> validatedUserOrError( Result<User> res, String pwd ) {
-		if( res.isOK())
-			return res.value().getPwd().equals( pwd ) ? res : error(FORBIDDEN);
+	private Result<User> validatedUserOrError(Result<User> res, String pwd) {
+		if (res.isOK())
+			return res.value().getPwd().equals(pwd) ? res : error(FORBIDDEN);
 		else
 			return res;
 	}
-	
-	private boolean badUserInfo( User user) {
+
+	private boolean badUserInfo(User user) {
 		return (user.userId() == null || user.pwd() == null || user.displayName() == null || user.email() == null);
 	}
-	
-	private boolean badUpdateUserInfo( String userId, String pwd, User info) {
-		return (userId == null || pwd == null || info.getUserId() != null && ! userId.equals( info.getUserId()));
+
+	private boolean badUpdateUserInfo(String userId, String pwd, User info) {
+		return (userId == null || pwd == null || info.getUserId() != null && !userId.equals(info.getUserId()));
 	}
 }
--- src/main/java/utils/DB.java
+++ src/main/java/utils/DB.java
@@ -8,37 +8,37 @@ import org.hibernate.Session;
 
 import tukano.api.Result;
 
-public class DB {
+public class DB { // interface
 
 	public static <T> List<T> sql(String query, Class<T> clazz) {
 		return Hibernate.getInstance().sql(query, clazz);
 	}
-	
-	public static <T> List<T> sql(Class<T> clazz, String fmt, Object ... args) {
+
+	public static <T> List<T> sql(Class<T> clazz, String fmt, Object... args) {
 		return Hibernate.getInstance().sql(String.format(fmt, args), clazz);
 	}
-	
+
 	public static <T> Result<T> getOne(String id, Class<T> clazz) {
-		return Hibernate.getInstance().getOne(id, clazz);
+		return Hibernate.getInstance().getOne(id, clazz); // get one container id string, class = User
 	}
-	
+
 	public static <T> Result<T> deleteOne(T obj) {
 		return Hibernate.getInstance().deleteOne(obj);
 	}
-	
+
 	public static <T> Result<T> updateOne(T obj) {
 		return Hibernate.getInstance().updateOne(obj);
 	}
-	
-	public static <T> Result<T> insertOne( T obj) {
+
+	public static <T> Result<T> insertOne(T obj) {
 		return Result.errorOrValue(Hibernate.getInstance().persistOne(obj), obj);
 	}
-	
-	public static <T> Result<T> transaction( Consumer<Session> c) {
-		return Hibernate.getInstance().execute( c::accept );
+
+	public static <T> Result<T> transaction(Consumer<Session> c) {
+		return Hibernate.getInstance().execute(c::accept);
 	}
-	
-	public static <T> Result<T> transaction( Function<Session, Result<T>> func) {
-		return Hibernate.getInstance().execute( func );
+
+	public static <T> Result<T> transaction(Function<Session, Result<T>> func) {
+		return Hibernate.getInstance().execute(func);
 	}
 }
