cbfea0fbbe0a3d9619cd77ed5c06b111f88308b5
Francisco <fpg.silva@campus.fct.unl.pt>
2024-11-03 20:26:33 +0000

    Hibernate?

--- src/main/java/tukano/impl/JavaUsers.java
+++ src/main/java/tukano/impl/JavaUsers.java
@@ -25,66 +25,37 @@ import utils.DB;
 import utils.JSON;
 
 public class JavaUsers implements Users {
-
+	
 	private static Logger Log = Logger.getLogger(JavaUsers.class.getName());
-	private static String MOST_RECENT_USERS_LIST = "MostRecentUsers";
-	private static Users instance;
 
+	private static Users instance;
+	
 	synchronized public static Users getInstance() {
-		if (instance == null)
+		if( instance == null )
 			instance = new JavaUsers();
 		return instance;
 	}
-
-	private JavaUsers() {
-	}
-
+	
+	private JavaUsers() {}
+	
 	@Override
 	public Result<String> createUser(User user) {
 		Log.info(() -> format("createUser : %s\n", user));
 
-		if (badUserInfo(user)){
-			System.out.println("BAD REQUEST CREATE USER");
-			return error(BAD_REQUEST);
-		}
-		
-		Log.info(() -> format("sporting: \n"));
+		if( badUserInfo( user ) )
+				return error(BAD_REQUEST);
 
-		
-		Result<User> dbResult = DB.insertOne(user);
-		/*if (dbResult.isOK()) {
-
-			try (Jedis jedis = RedisCache.getCachePool().getResource()) {
-				var key = "users:" + user.getUserId();
-				var value = JSON.encode(user);
-				jedis.set(key, value);
-
-				jedis.lpush(MOST_RECENT_USERS_LIST, value);
-				if (jedis.llen(MOST_RECENT_USERS_LIST) > 5) {
-					jedis.ltrim(MOST_RECENT_USERS_LIST, 0, 4);
-				}
-			}
-		}*/
-		return errorOrValue(dbResult, user.getUserId());
+		return errorOrValue( DB.insertOne( user), user.getUserId() );
 	}
 
 	@Override
 	public Result<User> getUser(String userId, String pwd) {
-		Log.info(() -> format("getUser : userId = %s, pwd = %s\n", userId, pwd));
+		Log.info( () -> format("getUser : userId = %s, pwd = %s\n", userId, pwd));
 
 		if (userId == null)
 			return error(BAD_REQUEST);
-
-		/*try (Jedis jedis = RedisCache.getCachePool().getResource()) {
-			var key = "users:" + userId;
-			var value = jedis.get(key);
-			if (value != null) {
-				User user = JSON.decode(value, User.class);
-				return validatedUserOrError(Result.ok(user), pwd);
-			}
-		}*/
-
-		return validatedUserOrError(DB.getOne(userId, User.class), pwd);
+		
+		return validatedUserOrError( DB.getOne( userId, User.class), pwd);
 	}
 
 	@Override
@@ -94,50 +65,34 @@ public class JavaUsers implements Users {
 		if (badUpdateUserInfo(userId, pwd, other))
 			return error(BAD_REQUEST);
 
-		return errorOrResult(validatedUserOrError(DB.getOne(userId, User.class), pwd),
-				user -> {
-					var updatedUser = user.updateFrom(other);
-
-					/*try (Jedis jedis = RedisCache.getCachePool().getResource()) {
-						var key = "users:" + userId;
-						var value = JSON.encode(updatedUser);
-						jedis.set(key, value);
-					}*/
-					return DB.updateOne(updatedUser);
-				});
+		return errorOrResult( validatedUserOrError(DB.getOne( userId, User.class), pwd), user -> DB.updateOne( user.updateFrom(other)));
 	}
 
 	@Override
 	public Result<User> deleteUser(String userId, String pwd) {
 		Log.info(() -> format("deleteUser : userId = %s, pwd = %s\n", userId, pwd));
 
-		if (userId == null || pwd == null)
+		if (userId == null || pwd == null )
 			return error(BAD_REQUEST);
 
-		return errorOrResult(validatedUserOrError(DB.getOne(userId, User.class), pwd), user -> {
+		return errorOrResult( validatedUserOrError(DB.getOne( userId, User.class), pwd), user -> {
 
 			// Delete user shorts and related info asynchronously in a separate thread
-			Executors.defaultThreadFactory().newThread(() -> {
+			Executors.defaultThreadFactory().newThread( () -> {
 				JavaShorts.getInstance().deleteAllShorts(userId, pwd, Token.get(userId));
 				JavaBlobs.getInstance().deleteAllBlobs(userId, Token.get(userId));
 			}).start();
-
-			/*try (Jedis jedis = RedisCache.getCachePool().getResource()) {
-				var key = "users:" + userId;
-				jedis.del(key);
-			}*/
-
-			return DB.deleteOne(user);
+			
+			return DB.deleteOne( user);
 		});
 	}
 
 	@Override
 	public Result<List<User>> searchUsers(String pattern) {
-		Log.info(() -> format("searchUsers : patterns = %s\n", pattern));
+		Log.info( () -> format("searchUsers : patterns = %s\n", pattern));
 
 		var query = format("SELECT * FROM User u WHERE UPPER(u.userId) LIKE '%%%s%%'", pattern.toUpperCase());
 		var hits = DB.sql(query, User.class)
-				.value()
 				.stream()
 				.map(User::copyWithoutPassword)
 				.toList();
@@ -145,18 +100,19 @@ public class JavaUsers implements Users {
 		return ok(hits);
 	}
 
-	private Result<User> validatedUserOrError(Result<User> res, String pwd) {
-		if (res.isOK())
-			return res.value().getPwd().equals(pwd) ? res : error(FORBIDDEN);
+	
+	private Result<User> validatedUserOrError( Result<User> res, String pwd ) {
+		if( res.isOK())
+			return res.value().getPwd().equals( pwd ) ? res : error(FORBIDDEN);
 		else
 			return res;
 	}
-
-	private boolean badUserInfo(User user) {
+	
+	private boolean badUserInfo( User user) {
 		return (user.userId() == null || user.pwd() == null || user.displayName() == null || user.email() == null);
 	}
-
-	private boolean badUpdateUserInfo(String userId, String pwd, User info) {
-		return (userId == null || pwd == null || info.getUserId() != null && !userId.equals(info.getUserId()));
+	
+	private boolean badUpdateUserInfo( String userId, String pwd, User info) {
+		return (userId == null || pwd == null || info.getUserId() != null && ! userId.equals( info.getUserId()));
 	}
 }
